c5e0c4775ff152a22932070724f285460829be77
Ntholi Nkhatho <ntholi@live.com>
2022-02-08 18:17:56 +0200

    Create 'ComboBox' component

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ import {
 import { Notification, FormClose, View, Github } from 'grommet-icons';
 import { grommet } from 'grommet/themes';
 import React, { useState } from 'react';
+import ComboBox from './components/ComboBox';
 
 const theme = {
   global: {
@@ -30,40 +31,7 @@ const theme = {
   },
 };
 
-// the prefix name of the Create option entry
-const prefix = 'Create';
-
-const defaultOptions: string[] = [];
-for (let i = 1; i <= 5; i += 1) {
-  defaultOptions.push(`option ${i}`);
-}
-
-const updateCreateOption = (text: string) => {
-  const len = defaultOptions.length;
-  if (defaultOptions[len - 1].includes(prefix)) {
-    // remove Create option before adding an updated one
-    defaultOptions.pop();
-  }
-  defaultOptions.push(`${prefix} '${text}'`);
-};
-
-// improving Search support of special characters
-const getRegExp = (text: string) => {
-  // The line below escapes regular expression special characters:
-  // [ \ ^ $ . | ? * + ( )
-  const escapedText = text.replace(/[-\\^$*+?.()|[\]{}]/g, '\\$&');
-
-  // Create the regular expression with modified value which
-  // handles escaping special characters. Without escaping special
-  // characters, errors will appear in the console
-  return new RegExp(escapedText, 'i');
-};
-
 function App() {
-  const [options, setOptions] = useState(defaultOptions);
-  const [value, setValue] = useState('');
-  const [searchValue, setSearchValue] = useState('');
-
   return (
     <Grommet full theme={grommet}>
       <Grid
@@ -82,29 +50,7 @@ function App() {
         <Box gridArea='nav' background='light-1'>
           <Box pad='medium' direction='row'>
             <FormField label='Lecturer' htmlFor='lecturer'>
-              <Select
-                open
-                size='medium'
-                placeholder='Select'
-                value={value}
-                options={options}
-                onChange={({ option }) => {
-                  if (option.includes(prefix)) {
-                    defaultOptions.pop(); // remove Create option
-                    defaultOptions.push(searchValue);
-                    setValue(searchValue);
-                  } else {
-                    setValue(option);
-                  }
-                }}
-                onClose={() => setOptions(defaultOptions)}
-                onSearch={(text: string) => {
-                  updateCreateOption(text);
-                  const exp = getRegExp(text);
-                  setOptions(defaultOptions.filter((o) => exp.test(o)));
-                  setSearchValue(text);
-                }}
-              />
+              <ComboBox />
             </FormField>
             <Button icon={<View size='medium' />} margin={{ top: 'medium' }} />
           </Box>
